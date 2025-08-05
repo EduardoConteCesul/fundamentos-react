e96383bb321e492a71740613a8ce8f8a38a04cd7
EduardoConteCesul <eduardo.2024014337@alunocesul.com>
2025-08-04 22:37:43 -0300

    Adicionado Input e Button

--- App.tsx
+++ App.tsx
@@ -1,4 +1,3 @@
-import { StyleSheet } from 'react-native';
 import { Home } from './src/screens/Home';
 
 export default function App() {
@@ -6,22 +5,3 @@ export default function App() {
     <Home />
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#2b34af',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  bottom: {
-    flex: 1,
-    backgroundColor: '#cc1212',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-  title: {
-    fontSize: 30,
-    fontWeight: 700
-  }
-});
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,9 +1,70 @@
-import { Text, View } from "react-native"
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native"
 
-export default function Home() {
+export function Home() {
   return (
-    <View>
-      <Text>Home</Text>
+    <View style={style.container}>
+      <Text style={style.title}>Lista de Compras</Text>
+      <Text>Quarta-feira, 17 de Julho de 2025</Text>
+      <View style={style.form}>
+        <TextInput
+          style={style.input}
+          placeholder="Nome do Produto"
+          placeholderTextColor="#BDBABA"
+          keyboardType="default"
+        />
+        <TouchableOpacity style={style.button}>
+          <Text style={style.textButton}>+</Text>
+        </TouchableOpacity>
+      </View>
+      <Text>Compras Pendentes</Text>
     </View>
   )
 }
+
+const style = StyleSheet.create({
+  container: {
+    flex: 1,
+
+    backgroundColor: "#EAEAEA",
+    padding: 24,
+  },
+  title: {
+    color: "#000",
+    fontSize: 24,
+    fontWeight: "bold",
+    lineHeight: 28.8,
+    marginTop: 48,
+  },
+  todayDate: {
+    color: "#000",
+    fontSize: 16,
+    fontWeight: "normal",
+    lineHeight: 28.8,
+  },
+  form: {
+    flexDirection: "row",
+    width: "100%",
+    marginTop: 18
+  },
+  input: {
+    flex: 1,
+    height: 56,
+    backgroundColor: "#FFF",
+    padding: 16,
+    borderRadius: 5,
+  },
+  button: {
+    width: 56,
+    height: 56,
+    backgroundColor: "#31C667",
+    borderRadius: 5,
+    marginLeft: 16,
+
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  textButton: {
+    color: "#FFF",
+    fontSize: 24
+  }
+})
